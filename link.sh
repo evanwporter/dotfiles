e1486e9910c5e847744572ec39e150d0ca4b7796
Evan Porter <evanwporter@gmail.com>
2026-06-21 14:36:20 +0000

    add yazi theme
    * add yazi plugin piper
    * add glow

--- link.sh
+++ link.sh
@@ -43,6 +43,7 @@ link_file "$DOTFILES_DIR/delta" "$HOME/.config/delta"
 link_file "$DOTFILES_DIR/bat" "$HOME/.config/bat"
 link_file "$DOTFILES_DIR/yazi" "$HOME/.config/yazi"
 link_file "$DOTFILES_DIR/nvimpager" "$HOME/.config/nvimpager"
+link_file "$DOTFILES_DIR/glow" "$HOME/.config/glow"
 
 echo
 echo "Done."
