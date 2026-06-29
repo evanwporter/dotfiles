2ea2bd148662b9481922525e2e8e6cce5f0567bd
Evan Porter <evanwporter@gmail.com>
2026-06-29 04:29:40 +0000

    use nvim-tree instead of neotree
    fix fff file picker

--- nvim/lua/plugins/fff.lua
+++ nvim/lua/plugins/fff.lua
@@ -4,18 +4,40 @@ return {
     build = function()
       require("fff.download").download_or_build_binary()
     end,
-    lazy = false, -- fff lazy-initialises itself
+    -- lazy = false,
+
+    init = function()
+      local function set_fff_highlights()
+        vim.api.nvim_set_hl(0, "FFFCursorLine", {
+          bg = "#504945",
+        })
+      end
+
+      set_fff_highlights()
+
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        callback = set_fff_highlights,
+      })
+    end,
+
     opts = {
       prompt = " ",
+
+      hl = {
+        cursor = "FFFCursorLine",
+      },
+
       debug = {
         enabled = false,
         show_scores = false,
       },
+
       preview = {
         enabled = true,
         cursorlineopt = "both",
       },
     },
+
     keys = {
       {
         "<leader><leader>",
