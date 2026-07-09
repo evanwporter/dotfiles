dcb2d2eaf62742925b89629bd62fe80bb68b4bb4
Evan Porter <evanwporter@gmail.com>
2026-07-09 01:35:59 +0000

    nix: complete rework

---
+++ nix/home/default.nix
@@ -0,0 +1,16 @@
+{
+	username,
+	homeDirectory,
+	...
+}: {
+	home = {
+		inherit username homeDirectory;
+		stateVersion = "26.05";
+	};
+
+	imports = [
+		./modules/cli.nix
+		./modules/dev.nix
+		./modules/neovim.nix
+	];
+}
