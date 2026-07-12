8325432ed0647630a44522958958c31f2756df12
Evan Porter <evanwporter@gmail.com>
2026-07-12 17:02:26 +0000

    nvim: move keymap 1-9 to harpoon and disable harpoon

--- nvim/lua/plugins/editor/harpoon2.lua
+++ nvim/lua/plugins/editor/harpoon2.lua
@@ -1,35 +1,45 @@
 return {
-    {
-        "ThePrimeagen/harpoon",
-        branch = "harpoon2",
-        opts = {
-            menu = {
-                width = vim.api.nvim_win_get_width(0) - 4,
-            },
-            settings = {
-                save_on_toggle = true,
-            },
-        },
-        keys = function()
-            local keys = {
-                {
-                    "<leader>H",
-                    function()
-                        require("harpoon"):list():add()
-                    end,
-                    desc = "Harpoon File",
-                },
-                {
-                    "<leader>h",
-                    function()
-                        local harpoon = require("harpoon")
-                        harpoon.ui:toggle_quick_menu(harpoon:list())
-                    end,
-                    desc = "Harpoon Quick Menu",
-                },
-            }
-
-            return keys
-        end,
-    },
+    -- {
+    --     "ThePrimeagen/harpoon",
+    --     branch = "harpoon2",
+    --     opts = {
+    --         menu = {
+    --             width = vim.api.nvim_win_get_width(0) - 4,
+    --         },
+    --         settings = {
+    --             save_on_toggle = true,
+    --         },
+    --     },
+    --     keys = function()
+    --         local keys = {
+    --             {
+    --                 "<leader>H",
+    --                 function()
+    --                     require("harpoon"):list():add()
+    --                 end,
+    --                 desc = "Harpoon File",
+    --             },
+    --             {
+    --                 "<leader>h",
+    --                 function()
+    --                     local harpoon = require("harpoon")
+    --                     harpoon.ui:toggle_quick_menu(harpoon:list())
+    --                 end,
+    --                 desc = "Harpoon Quick Menu",
+    --             },
+    --         }
+    --
+    --         return keys
+    --     end,
+    --     config = function(_, opts)
+    --         local harpoon = require("harpoon")
+    --         harpoon:setup(opts)
+    --
+    --         for i = 1, 9 do
+    --             vim.keymap.set("n", "<leader>" .. i, function()
+    --                 require("harpoon"):list():select(i)
+    --             end, { desc = "which_key_ignore" })
+    --         end
+    --     end,
+    -- },
 }
