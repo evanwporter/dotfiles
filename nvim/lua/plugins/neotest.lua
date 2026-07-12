e05d8d5eb95080581d4a1ce07dab2f74268be6c6
Evan Porter <evanwporter@gmail.com>
2026-07-12 16:52:35 +0000

    nvim: rework keymaps + remove unused plugins

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -7,55 +7,55 @@ return {
             "evanwporter/neotest-ctest",
         },
         keys = {
+            -- {
+            --     "<leader>tn",
+            --     function()
+            --         require("neotest").run.run()
+            --     end,
+            --     desc = "Run nearest test",
+            -- },
+            -- {
+            --     "<leader>tf",
+            --     function()
+            --         require("neotest").run.run(vim.fn.expand("%"))
+            --     end,
+            --     desc = "Run test file",
+            -- },
+            -- {
+            --     "<leader>td",
+            --     function()
+            --         require("neotest").run.run({ strategy = "dap" })
+            --     end,
+            --     desc = "Debug nearest test",
+            -- },
             {
-                "<leader>tn",
-                function()
-                    require("neotest").run.run()
-                end,
-                desc = "Run nearest test",
-            },
-            {
-                "<leader>tf",
-                function()
-                    require("neotest").run.run(vim.fn.expand("%"))
-                end,
-                desc = "Run test file",
-            },
-            {
-                "<leader>td",
-                function()
-                    require("neotest").run.run({ strategy = "dap" })
-                end,
-                desc = "Debug nearest test",
-            },
-            {
-                "<leader>ts",
+                "<leader>tt",
                 function()
                     require("neotest").summary.toggle()
                 end,
                 desc = "Toggle test summary",
             },
-            {
-                "<leader>to",
-                function()
-                    require("neotest").output.open({ enter = true })
-                end,
-                desc = "Open test output",
-            },
-            {
-                "<leader>tO",
-                function()
-                    require("neotest").output_panel.toggle()
-                end,
-                desc = "Toggle test output panel",
-            },
-            {
-                "<leader>tS",
-                function()
-                    require("neotest").run.stop()
-                end,
-                desc = "Stop test",
-            },
+            -- {
+            --     "<leader>to",
+            --     function()
+            --         require("neotest").output.open({ enter = true })
+            --     end,
+            --     desc = "Open test output",
+            -- },
+            -- {
+            --     "<leader>tO",
+            --     function()
+            --         require("neotest").output_panel.toggle()
+            --     end,
+            --     desc = "Toggle test output panel",
+            -- },
+            -- {
+            --     "<leader>tS",
+            --     function()
+            --         require("neotest").run.stop()
+            --     end,
+            --     desc = "Stop test",
+            -- },
         },
         config = function()
             local neotest = require("neotest")
