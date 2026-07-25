3f063d00b80707030865e7430d5a34db0a5547c0
Evan Porter <evanwporter@gmail.com>
2026-07-25 11:09:48 -0700

    nvim: add cmake `if`/`endif` to `vaf` (@function)

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -6,65 +6,28 @@ return {
         event = "BufReadPost",
 
         init = function()
+            -- Disable entire built-in ftplugin mappings to avoid conflicts.
+            -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
             vim.g.no_plugin_maps = true
         end,
-
-        config = function()
-            -- require("nvim-treesitter-textobjects").setup({
-            --   select = {
-            --     lookahead = true,
-            --     selection_modes = {
-            --       ["@parameter.inner"] = "v",
-            --       ["@parameter.outer"] = "v",
-            --       ["@function.inner"] = "V",
-            --       ["@function.outer"] = "V",
-            --       ["@comment.outer"] = "V",
-            --     },
-            --     include_surrounding_whitespace = false,
-            --   },
-            --
-            --   move = {
-            --     set_jumps = true,
-            --   },
-            -- })
+        config = function(_, opts)
+            require("nvim-treesitter-textobjects").setup(opts)
 
             local select = require("nvim-treesitter-textobjects.select")
 
-            vim.keymap.set({ "x", "o" }, "af", function()
-                select.select_textobject("@function.outer", "textobjects")
-            end, {
-                silent = true,
-                desc = "Around function",
-            })
+            local function textobject(lhs, query, desc)
+                vim.keymap.set({ "x", "o" }, lhs, function()
+                    select.select_textobject(query, "textobjects")
+                end, {
+                    silent = true,
+                    desc = desc,
+                })
+            end
 
-            vim.keymap.set({ "x", "o" }, "if", function()
-                select.select_textobject("@function.inner", "textobjects")
-            end, {
-                silent = true,
-                desc = "Inside function",
-            })
-
-            -- Use aa/ia for arguments so ap/ip retain paragraph behavior.
-            vim.keymap.set({ "x", "o" }, "aa", function()
-                select.select_textobject("@parameter.outer", "textobjects")
-            end, {
-                silent = true,
-                desc = "Around argument",
-            })
-
-            vim.keymap.set({ "x", "o" }, "ia", function()
-                select.select_textobject("@parameter.inner", "textobjects")
-            end, {
-                silent = true,
-                desc = "Inside argument",
-            })
-
-            vim.keymap.set({ "x", "o" }, "ac", function()
-                select.select_textobject("@comment.outer", "textobjects")
-            end, {
-                silent = true,
-                desc = "Around comment",
-            })
+            textobject("af", "@function.outer", "Around function")
+            textobject("if", "@function.inner", "Inside function")
+            textobject("ac", "@conditional.outer", "Around conditional")
+            textobject("ic", "@conditional.inner", "Inside conditional")
         end,
     },
 }
