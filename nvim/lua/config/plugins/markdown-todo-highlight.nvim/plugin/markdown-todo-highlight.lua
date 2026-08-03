76a6f54b4c9cec7f891e18d3b6ac383229752532
Evan Porter <evanwporter@gmail.com>
2026-08-02 19:42:41 -0700

    nvim: add marksman lsp + get rid of custom markdown todo highlights

--- nvim/lua/config/plugins/markdown-todo-highlight.nvim/plugin/markdown-todo-highlight.lua
+++
@@ -1,4 +0,0 @@
-if vim.g.loaded_markdown_todo_highlight then
-    return
-end
-vim.g.loaded_markdown_todo_highlight = true
--- nvim/lua/plugins/languages/markdown.lua
+++ nvim/lua/plugins/languages/markdown.lua
@@ -5,10 +5,4 @@ return {
         opts = {},
         ft = { "markdown" },
     },
-    {
-        name = "markdown-todo-highlight",
-        dir = vim.fn.stdpath("config") .. "/lua/config/plugins/markdown-todo-highlight.nvim",
-        opts = {},
-        ft = { "markdown" },
-    },
 }
