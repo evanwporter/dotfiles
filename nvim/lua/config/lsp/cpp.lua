7d944c0aec9d4aa8ae1bfbc1aa8eed6b1684847d
Evan Porter <evanwporter@gmail.com>
2026-08-15 11:18:04 -0700

    nvim: add devkit arm query driver

--- nvim/lua/config/lsp/cpp.lua
+++ nvim/lua/config/lsp/cpp.lua
@@ -4,7 +4,7 @@ vim.lsp.config("clangd", {
         "--clang-tidy",
         "--completion-style=detailed",
         "--header-insertion=iwyu",
-        "--query-driver=/run/current-system/sw/bin/clang++,/run/current-system/sw/bin/g++,/nix/store/*/bin/clang++,/nix/store/*/bin/g++",
+        "--query-driver=/run/current-system/sw/bin/clang++,/run/current-system/sw/bin/g++,/nix/store/*/bin/clang++,/nix/store/*/bin/g++,/nix/store/*devkitARM*/opt/devkitpro/devkitARM/bin/arm-none-eabi-*",
     },
 })
 vim.lsp.enable("clangd")
