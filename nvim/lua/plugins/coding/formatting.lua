cfeae9124a9156e00da6b90dca80595d2e882d53
Evan Porter <evanwporter@gmail.com>
2026-08-16 09:33:23 -0700

    nvim: use nixfmt in nixpkgs

--- nvim/lua/plugins/coding/formatting.lua
+++ nvim/lua/plugins/coding/formatting.lua
@@ -1,3 +1,15 @@
+local function is_nixpkgs(bufnr)
+    local filename = vim.api.nvim_buf_get_name(bufnr)
+    local root = vim.fs.root(filename, {
+        "pkgs/top-level/all-packages.nix",
+        "maintainers/maintainer-list.nix",
+    })
+
+    return root ~= nil
+        and vim.uv.fs_stat(root .. "/pkgs/top-level/all-packages.nix") ~= nil
+        and vim.uv.fs_stat(root .. "/maintainers/maintainer-list.nix") ~= nil
+end
+
 return {
     {
         "stevearc/conform.nvim",
@@ -20,7 +32,10 @@ return {
                 json5 = { "prettier" },
                 yaml = { "prettier" },
 
-                nix = { "alejandra" },
+                nix = function(bufnr)
+                    return { is_nixpkgs(bufnr) and "nixfmt" or "alejandra" }
+                end,
+
                 cmake = { "cmake_format" },
 
                 xml = { "xmlstarlet" },
