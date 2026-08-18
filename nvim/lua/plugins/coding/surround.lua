0e779d3ec0bc254dc30f12b50da1c36988ccd702
Evan Porter <evanwporter@gmail.com>
2026-08-18 08:07:32 -0700

    nvim: mini.surround -> nvim-surround

--- nvim/lua/plugins/coding/surround.lua
+++ nvim/lua/plugins/coding/surround.lua
@@ -1,22 +1,16 @@
 return {
     {
-        "echasnovski/mini.surround",
+        "kylechui/nvim-surround",
+        init = function()
+            -- Disable default keymaps
+            vim.g.nvim_surround_no_mappings = true
+        end,
         keys = {
-            { "sa", desc = "Add surrounding", mode = { "n", "x" } },
-            { "sd", desc = "Delete surrounding" },
-            { "sr", desc = "Replace surrounding" },
-            { "sh", desc = "Highlight surrounding" },
+            { "sa", "<Plug>(nvim-surround-normal)", mode = "n", desc = "Add surround" },
+            { "sd", "<Plug>(nvim-surround-delete)", mode = "n", desc = "Delete surround" },
+            { "sr", "<Plug>(nvim-surround-change)", mode = "n", desc = "Replace surround" },
+            { "sa", "<Plug>(nvim-surround-visual)", mode = "x", desc = "Add surround" },
         },
-        opts = {
-            mappings = {
-                add = "sa",
-                delete = "sd",
-                find = "",
-                find_left = "",
-                highlight = "sh",
-                replace = "sr",
-                update_n_lines = "",
-            },
-        },
+        opts = {},
     },
 }
