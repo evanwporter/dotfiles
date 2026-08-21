b6f8559986806f902a23703eb8d4d1b2f17387b0
Evan Porter <evanwporter@gmail.com>
2026-08-20 22:36:50 -0700

    sway: remove fuzzle

--- nix/modules/sway.nix
+++ nix/modules/sway.nix
@@ -2,16 +2,15 @@
 	programs.sway = {
 		enable = true;
 		wrapperFeatures.gtk = true;
-	};
 
-	environment.systemPackages = with pkgs; [
-		brightnessctl
-		fuzzel
-		pavucontrol
-		swayidle
-		swaylock
-		waybar
-	];
+		extraPackages = with pkgs; [
+			brightnessctl
+			pavucontrol
+			swayidle
+			swaylock
+			waybar
+		];
+	};
 
 	services.displayManager.sddm = {
 		enable = true;
