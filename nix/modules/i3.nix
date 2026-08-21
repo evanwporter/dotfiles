0c89dec4e1668eedeea652672c2ad1730797018c
Evan Porter <evanwporter@gmail.com>
2026-08-20 18:34:45 -0700

    nix: configure i3

--- nix/modules/i3.nix
+++ nix/modules/i3.nix
@@ -1,12 +1,21 @@
 {pkgs, ...}: {
 	environment.systemPackages = with pkgs; [
 		polybar
+		pavucontrol
+		libinput-gestures
 	];
 
 	services.xserver = {
 		enable = true;
+		# Scale the entire X11 desktop to roughly 150% (96 DPI is 100%).
+		dpi = 192;
 
 		windowManager.i3.enable = true;
+	};
+
+	services.libinput = {
+		enable = true;
+		touchpad.naturalScrolling = false;
 	};
 
 	services.displayManager.sddm.enable = true;
