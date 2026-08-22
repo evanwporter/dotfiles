086207f882818b78e1f94136b506ce108f619fb8
Evan Porter <evanwporter@gmail.com>
2026-08-22 11:01:40 -0700

    nix: use config.fish instead of hacky sourcing

--- nix/home/base/modules/fish.nix
+++ nix/home/base/modules/fish.nix
@@ -1,13 +1,11 @@
 {
 	pkgs,
 	config,
+	lib,
 	...
 }: {
 	programs.fish = {
 		enable = true;
-		shellInit = ''
-			source ${config.home.homeDirectory}/dotfiles/fish/config.fish
-		'';
 		plugins = with pkgs.fishPlugins; [
 			{
 				name = "bobthefish";
@@ -16,7 +14,9 @@
 		];
 	};
 
-	# Fish creates this file on first launch. Allow Home Manager to take
-	# ownership of an existing, unmanaged copy during activation.
-	xdg.configFile."fish/config.fish".force = true;
+	# Use the repository's config.fish instead of Home Manager's generated one.
+	xdg.configFile."fish/config.fish" = {
+		force = true;
+		source = lib.mkForce "${config.home.homeDirectory}/dotfiles/fish/config.fish";
+	};
 }
