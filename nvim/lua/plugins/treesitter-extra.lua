b27499080cf34fce0d439f787df0e9c9dce0e46e
Evan Porter <evanwporter@gmail.com>
2026-08-23 21:10:27 -0700

    nvim: add vim-matchup

--- nvim/lua/plugins/treesitter-extra.lua
+++ nvim/lua/plugins/treesitter-extra.lua
@@ -1,5 +1,14 @@
 return {
     {
+        "andymass/vim-matchup",
+        lazy = false,
+        init = function()
+            -- Combine Tree-sitter delimiters with filetype-specific
+            -- `b:match_words` pairs, including SystemVerilog begin/end.
+            vim.g.matchup_treesitter_include_match_words = true
+        end,
+    },
+    {
         "nvim-treesitter/nvim-treesitter-textobjects",
         branch = "main",
         lazy = true,
