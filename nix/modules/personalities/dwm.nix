c0e2ac1a52bebbd318acad31516d0ca18101e8b0
Evan Porter <evanwporter@gmail.com>
2026-08-30 21:41:47 -0700

    nix: use kitty instead of st

--- nix/modules/personalities/dwm.nix
+++ nix/modules/personalities/dwm.nix
@@ -1,12 +1,12 @@
 {inputs, ...}: {
 	flake.modules.nixos.personality-dwm = {
 		imports = with inputs.self.modules.nixos; [system-desktop terminal dwm ly browser];
-		terminal.default = "st";
+		terminal.default = "kitty";
 		home-manager.sharedModules = [inputs.self.modules.homeManager.personality-dwm];
 	};
 
 	flake.modules.homeManager.personality-dwm = {
-		# imports = with inputs.self.modules.homeManager; [i3 rofi];
+		imports = with inputs.self.modules.homeManager; [dwm];
 		development.vscode.enable = true;
 	};
 }
