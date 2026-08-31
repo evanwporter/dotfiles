ebcaa9972ffff8e0bc822439e0985b2a750b3687
Evan Porter <evanwporter@gmail.com>
2026-08-30 21:41:25 -0700

    nix: link dunst

--- nix/modules/home/dotfiles.nix
+++ nix/modules/home/dotfiles.nix
@@ -25,6 +25,7 @@
 			alejandra = linkWhen (hasPackage pkgs.alejandra) "alejandra";
 			bat = linkWhen (hasPackage pkgs.bat) "bat";
 			delta = linkWhen (config.programs.delta.enable || hasPackage pkgs.delta) "delta";
+			dunst = linkWhen (hasPackage pkgs.dunst) "dunst";
 			erdtree = linkWhen (hasPackage pkgs.erdtree) "erdtree";
 			kitty = linkWhen (hasPackage pkgs.kitty) "kitty";
 			lazygit = linkWhen (hasPackage pkgs.lazygit) "lazygit";
