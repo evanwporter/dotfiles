2b925704404cf5a657065b4f5db56a587b9117f4
Evan Porter <evanwporter@gmail.com>
2026-09-06 12:14:39 -0700

    pi: add lsps

--- nix/modules/programs/development/pi.nix
+++ nix/modules/programs/development/pi.nix
@@ -1,9 +1,14 @@
-{dotfilesRoot, ...}: {
+{...}: {
 	flake.modules.homeManager.pi = {pkgs, ...}: let
+		language_servers = with pkgs; [
+			clang-tools
+			pyrefly
+			slang-server
+		];
 	in {
 		programs.pi-coding-agent = {
 			enable = true;
-			extraPackages = with pkgs; [nodejs bun rtk];
+			extraPackages = with pkgs; [nodejs bun rtk] ++ language_servers;
 		};
 
 		# home.file.".pi".source = osConfig._module.args.impurity.link piConfig;
