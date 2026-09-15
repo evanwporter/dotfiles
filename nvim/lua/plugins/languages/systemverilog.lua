cbf3c8c0c3865c5e99f312e15b06921d28529972
Evan Porter <evanwporter@gmail.com>
2026-09-14 22:31:04 -0700

    nvim: disable slang-server

--- nvim/lua/plugins/languages/systemverilog.lua
+++ nvim/lua/plugins/languages/systemverilog.lua
@@ -1,6 +1,7 @@
 return {
     {
         "hudson-trading/slang-server.nvim",
+        enabled = false,
         cond = not vim.g.vscode,
         ft = { "verilog", "systemverilog" },
         -- cmd = { "SlangServer" },
