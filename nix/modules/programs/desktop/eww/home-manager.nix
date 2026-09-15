f368d9d26559574d868a2c1e00549155fb1ca443
Evan Porter <evanwporter@gmail.com>
2026-09-14 22:28:50 -0700

    eww: add chadwm config

---
+++ nix/modules/programs/desktop/eww/home-manager.nix
@@ -0,0 +1,18 @@
+{inputs, ...}: {
+	flake.modules.homeManager.eww = {pkgs, ...}: {
+		programs.eww = {
+			enable = true;
+			package = pkgs.eww;
+			yuckConfig = builtins.readFile ./eww/eww.yuck;
+			scssConfig = builtins.readFile ./eww/eww.scss;
+		};
+
+		xdg.configFile = {
+			"eww/var.yuck".source = ./eww/var.yuck;
+			"eww/src".source = ./eww/src;
+			"eww/scss".source = ./eww/scss;
+			"eww/scripts".source = ./eww/scripts;
+			"eww/config".source = ./eww/config;
+		};
+	};
+}
