2e1045f39be0c51e8863f22f547332d72111d005
Evan Porter <evanwporter@gmail.com>
2026-09-26 11:36:04 -0700

    nix: add picom animation rules
    goes along with dwm conveyor mode
    bumped dwm git module to get the conveyor mode

--- nix/modules/programs/desktop/common/picom/picom.nix
+++ nix/modules/programs/desktop/common/picom/picom.nix
@@ -1,7 +1,15 @@
-{packagesDir, ...}: {
-	flake.modules.nixos.picom = {pkgs, ...}: let
+{
+	inputs,
+	packagesDir,
+	...
+}: {
+	flake.modules.nixos.picom = {
+		lib,
+		pkgs,
+		...
+	}: let
 		picom =
-			pkgs.picom.overrideAttrs (old: {
+			pkgs.picom.overrideAttrs (_: {
 					src = packagesDir + "/picom";
 				});
 	in {
@@ -11,6 +19,14 @@
 			backend = "glx";
 			vSync = true;
 		};
+
+		# Do not pass NixOS's generated config. Picom discovers the Home
+		# Manager-linked config at $XDG_CONFIG_HOME/picom/picom.conf instead.
+		systemd.user.services.picom.serviceConfig.ExecStart =
+			lib.mkForce
+			"${lib.getExe picom} --backend glx";
+
+		home-manager.sharedModules = [inputs.self.modules.homeManager.picom];
 	};
 
 	flake.modules.homeManager.picom = {
