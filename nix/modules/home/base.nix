8c3d4b6f46d5f33bc3d480f9700e986b7b426ed6
Evan Porter <evanwporter@gmail.com>
2026-09-26 11:36:28 -0700

    nix: cleaned up home/base.nix

--- nix/modules/home/base.nix
+++ nix/modules/home/base.nix
@@ -2,7 +2,6 @@
 	flake.modules.homeManager.home-default = {
 		imports = [
 			inputs.self.modules.homeManager.dotfiles
-			# inputs.self.modules.homeManager.cursor
 		];
 		home.stateVersion = "26.05";
 	};
